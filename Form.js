5f0ae06903baa90cd58518dc93a736f058ed06c1
Ankita3107 <63492100+Ankita3107@users.noreply.github.com>
2020-06-01 20:08:57 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,32 @@
+class Form {
+  constructor() {    
+  }
+
+  display(){
+    fill(255);
+    var title = createElement('h2')
+    title.html("Painter's Choice");
+    title.position(130, 0);
+    
+    var input = createInput("Name");
+    var button = createButton('Create');
+    input.position(130, 160);
+    button.position(250, 200);
+
+    button.mousePressed(function(){
+      input.hide();
+      button.hide();
+      title.hide();
+      
+      var name = input.value();
+      
+      playerCount+=1;
+      player.update(name)
+      player.updateCount(playerCount);
+      var greeting = createElement('h3');
+      //greeting.html("Hello " + name )
+      greeting.position(130, 160)
+    });
+
+  }
+}
